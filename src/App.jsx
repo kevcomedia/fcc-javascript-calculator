a8a174f726e4ecdb8dcb77f1bcfb3b33ed8912c2
Kev Comedia <kevcomedia@gmail.com>
2022-03-13 19:39:03 +0800

    Wrap display state in object

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,57 @@ import calculatorService from './services/calculatorService'
 const reducer = (state, action) => {
   switch (action.type) {
     case 'clear':
-      return '0'
+      return { display: '0' }
     case 'digit':
-      if (state === '0') {
-        return action.digit
+      if (state.display === '0') {
+        return { display: action.digit }
       } else {
-        return state + action.digit
+        return { display: state.display + action.digit }
       }
     case 'decimal':
-      if (/\.\d*$/.test(state)) {
+      if (/\.\d*$/.test(state.display)) {
         // Prevent more than one decimal point per number
         return state
-      } else if (/[-+*/]$/.test(state)) {
-        return state + '0.'
+      } else if (/[-+*/]$/.test(state.display)) {
+        return { display: state.display + '0.' }
       } else {
-        return state + '.'
+        return { display: state.display + '.' }
       }
     case '+':
     case '*':
     case '/':
-      return state.replace(/^(.*(\d|\.))([-+*/]-?)?$/, '$1' + action.type)
+      return {
+        display: state.display.replace(
+          /^(.*(\d|\.))([-+*/]-?)?$/,
+          '$1' + action.type
+        ),
+      }
     case '-':
-      if (/[-+*/]-$/.test(state)) {
+      if (/[-+*/]-$/.test(state.display)) {
         return state
       } else {
-        return state + '-'
+        return { display: state.display + '-' }
       }
     case 'equals':
       // Show only at most 10 decimal places, but trim any trailing zeroes.
-      return `${+calculatorService.evaluateExpression(state).toPrecision(10)}`
+      return {
+        display: `${+calculatorService
+          .evaluateExpression(state.display)
+          .toPrecision(10)}`,
+        isFreshEval: true,
+      }
   }
 }
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, '0')
+  const [state, dispatch] = useReducer(reducer, {
+    display: '0',
+  })
 
   return (
     // temporary margin so the test menu doesn't block the calculator
     <div className="m-16 p-4 w-80 bg-zinc-700">
-      <Display className="mb-2" display={state} />
+      <Display className="mb-2" display={state.display} />
       <Buttons dispatch={dispatch} />
     </div>
   )
